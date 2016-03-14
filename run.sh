597f328bc7d07ac34bf740b3ff28ae1c96809e45
Mert Ergun <mert.rgun@gmail.com>
2016-03-14 15:19:49 +0100

    initial commit #3

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+screen -dmS test bash -c 'PYSPARK_DRIVER_PYTHON=jupyter PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --ip=* --port=8080" /home/mertergun/Desktop/code/spark-1.4.1-bin-hadoop2.6/./bin/pyspark --driver-memory 2g; exec bash'
