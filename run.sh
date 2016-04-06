1d28b64b3bbad5969d4cfcc9148faa42ef43aed2
mertergun <mert.rgun@gmail.com>
2016-04-06 11:48:01 +0200

    Update run.sh for server

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export YAHOO_DATA=/home/mertergun/yahoo_data/
-screen -dmS test bash -c 'PYSPARK_DRIVER_PYTHON=jupyter PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --ip=* --port=8080" /home/mertergun/Desktop/code/spark-1.4.1-bin-hadoop2.6/./bin/pyspark --driver-memory 2g; exec bash'
+export YAHOO_DATA=/home/mert/yahoo/
+screen -dmS test bash -c 'PYSPARK_DRIVER_PYTHON=jupyter PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --ip=* --port=8080" /home/mert/spark-1.4.1-bin-hadoop2.6/./bin/pyspark --driver-memory 2g; exec bash'
